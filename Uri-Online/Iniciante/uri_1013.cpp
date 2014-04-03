70c54bac5175b8e5b541457e7b3456da8c58e8a5
phmacedoaraujo <phmacedoaraujo@gmail.com>
2014-04-03 19:17:43 -0300

    Create uri_1013.cpp

---
+++ Uri-Online/Iniciante/uri_1013.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+
+/* Funcao recebe tres numeros inteiros e retorna o maior deles */
+
+int calculaMaior(int a, int b, int c){
+
+
+
+    return 0;
+}
+
+int main(){
+
+	int a, b, c, maior;
+	cin >> a >> b >> c;
+
+	maior = calculaMaior(a,b,c);
+
+	cout << maior << " eh o maior\n";
+
+	return 0;
+}
